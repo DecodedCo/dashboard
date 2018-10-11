5937ffd13f7f4b42bc56635de67d4d06fe7c060f
Harriet Pellereau <harrietp@users.noreply.github.com>
2018-10-11 23:45:12 +0100

    Created nps.js

---
+++ nps.js
@@ -0,0 +1,25 @@
+// NPS 
+$.getJSON( "https://sheetsu.com/apis/v1.0su/18195639e36f", function( data ) {
+  var scores = []
+  $.each( data, function( key, val ) {
+     scores.push(val.nps)
+  });
+  
+  var totalNps = nps(scores);
+  $('#nps').html(totalNps + '<span class="label">NPS</span>')
+  
+
+});
+
+// Copied directly From https://gist.github.com/mzabriskie/1ee9a8865d70e900f4e3
+function nps(scores) {
+  var promoters = 0;
+  var detractors = 0;
+
+  for (var i=0, l=scores.length; i<l; i++) {
+    if (scores[i] >= 9) promoters++;
+    if (scores[i] <= 6) detractors++;
+  }
+
+  return Math.round(((promoters / l) - (detractors / l)) * 100);
+}
