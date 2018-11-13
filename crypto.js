bae41423513dc64816e65a9e66c4924c73939ab7
Harriet Pellereau <harrietp@users.noreply.github.com>
2018-11-13 23:08:38 +0000

    Update crypto.js

--- crypto.js
+++ crypto.js
@@ -1,13 +1,11 @@
-// Adapted from https://api.jquery.com/jquery.getjson/
-
-// CHALLENGE:
-// Replace this simple tracker with a full-blown tracker widget from https://www.cryptocompare.com/dev/widget/wizard
-
+// Define cryptocurrency
 var cryptoSymbol = 'BTC'
+// Define currency for comparison
 var cryptoForex = 'GBP'
+// Get currency data
 $.getJSON("https://min-api.cryptocompare.com/data/price?fsym=" + cryptoSymbol + "&tsyms=" + cryptoForex, function( data ) {
+  // Capture currency data sentance in a variable called cryptoText
   var cryptoText = cryptoSymbol + " 1 = " +  cryptoForex + " " + data[cryptoForex]
-
+  // Push cryptoText into any html item labelled crypto 
   $('#crypto').html(cryptoText)
-
 });
