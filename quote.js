851edbdf2cf65145dc491c515e2022f2005087bd
Harriet Pellereau <harrietp@users.noreply.github.com>
2019-01-23 16:05:51 +0000

    Update quote.js

--- quote.js
+++ quote.js
@@ -1,7 +1,10 @@
-// Adapted from https://api.jquery.com/jquery.getjson/
-$.getJSON("https://quotes.rest/qod.json?category=inspire", function( data ) {
-  var quoteText = data.contents.quotes[0].quote
- 
-  $('#quote').html(quoteText)
-
-});
+// Select a random quote from data
+var rand = data.quotes[Math.floor(Math.random() * data.quotes.length)];
+// Capture the random quote text in a new variable called quoteText
+var quoteText = rand.quote;
+// Capture the random quote author in a new variable called authorText
+var authorText = rand.author;
+// Put quoteText content inside an html object labelled quote
+$('#quote').html(quoteText); 
+// Put author content inside an html object labelled author
+$('#author').html(authorText);
